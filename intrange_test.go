7d9f01c5e77af13d09930c68c93a49b217473665
Clifton Kaznocha <ckaznocha@gmail.com>
2025-03-24 21:23:21 -0700

    Add tests for identEqual (#72)

--- intrange_test.go
+++ intrange_test.go
@@ -6,6 +6,131 @@ import (
 	"testing"
 )
 
+func TestIdentEqual(t *testing.T) {
+	tests := []struct {
+		a    ast.Expr
+		b    ast.Expr
+		name string
+		want bool
+	}{
+		{
+			name: "Both nil",
+			a:    nil,
+			b:    nil,
+			want: false,
+		},
+		{
+			name: "One nil, one non-nil",
+			a:    &ast.Ident{Name: "x"},
+			b:    nil,
+			want: false,
+		},
+		{
+			name: "Both Ident with same name",
+			a:    &ast.Ident{Name: "x"},
+			b:    &ast.Ident{Name: "x"},
+			want: true,
+		},
+		{
+			name: "Both Ident with different names",
+			a:    &ast.Ident{Name: "x"},
+			b:    &ast.Ident{Name: "y"},
+			want: false,
+		},
+		{
+			name: "SelectorExpr with matching structure",
+			a: &ast.SelectorExpr{
+				X:   &ast.Ident{Name: "pkg"},
+				Sel: &ast.Ident{Name: "Func"},
+			},
+			b: &ast.SelectorExpr{
+				X:   &ast.Ident{Name: "pkg"},
+				Sel: &ast.Ident{Name: "Func"},
+			},
+			want: true,
+		},
+		{
+			name: "SelectorExpr with different structure",
+			a: &ast.SelectorExpr{
+				X:   &ast.Ident{Name: "pkg1"},
+				Sel: &ast.Ident{Name: "Func"},
+			},
+			b: &ast.SelectorExpr{
+				X:   &ast.Ident{Name: "pkg2"},
+				Sel: &ast.Ident{Name: "Func"},
+			},
+			want: false,
+		},
+		{
+			name: "IndexExpr with matching structure",
+			a: &ast.IndexExpr{
+				X:     &ast.Ident{Name: "arr"},
+				Index: &ast.BasicLit{Kind: token.INT, Value: "0"},
+			},
+			b: &ast.IndexExpr{
+				X:     &ast.Ident{Name: "arr"},
+				Index: &ast.BasicLit{Kind: token.INT, Value: "0"},
+			},
+			want: true,
+		},
+		{
+			name: "IndexExpr with different structure",
+			a: &ast.IndexExpr{
+				X:     &ast.Ident{Name: "arr1"},
+				Index: &ast.BasicLit{Kind: token.INT, Value: "0"},
+			},
+			b: &ast.IndexExpr{
+				X:     &ast.Ident{Name: "arr2"},
+				Index: &ast.BasicLit{Kind: token.INT, Value: "0"},
+			},
+			want: false,
+		},
+		{
+			name: "BasicLit with same value",
+			a:    &ast.BasicLit{Kind: token.INT, Value: "42"},
+			b:    &ast.BasicLit{Kind: token.INT, Value: "42"},
+			want: true,
+		},
+		{
+			name: "BasicLit with different value",
+			a:    &ast.BasicLit{Kind: token.INT, Value: "42"},
+			b:    &ast.BasicLit{Kind: token.INT, Value: "43"},
+			want: false,
+		},
+		{
+			name: "Different types of expressions",
+			a:    &ast.BasicLit{Kind: token.INT, Value: "42"},
+			b:    &ast.Ident{Name: "x"},
+			want: false,
+		},
+		{
+			name: "Different types of expressions reversed",
+			a:    &ast.Ident{Name: "x"},
+			b:    &ast.BasicLit{Kind: token.INT, Value: "42"},
+			want: false,
+		},
+		{
+			name: "Incompatible types",
+			a: &ast.CallExpr{
+				Fun: &ast.Ident{Name: "int"},
+				Args: []ast.Expr{
+					&ast.BasicLit{Kind: token.INT, Value: "42"},
+				},
+			},
+			b:    &ast.BasicLit{Kind: token.INT, Value: "42"},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if result := identEqual(tt.a, tt.b); result != tt.want {
+				t.Errorf("identEqual(%v, %v) = %v; want %v", tt.a, tt.b, result, tt.want)
+			}
+		})
+	}
+}
+
 func TestIsNumberLit(t *testing.T) {
 	tests := []struct {
 		expr ast.Expr
